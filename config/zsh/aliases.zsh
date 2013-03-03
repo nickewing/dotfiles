107d65f0adf7aaa1852de526a2c83c2ad36286f9
Nick Ewing <nick@nickewing.net>
2013-03-02 22:26:55 -0800

    Remove unwanted aliases

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -2,9 +2,6 @@
 if [ `uname` = "Darwin" ]; then # OS X specific 
 	alias ls='ls -Ghl '
   alias e='mvim'
-  gemacs() {
-    /Applications/Emacs.app/Contents/MacOS/Emacs "$@" &
-  }
 else # Non OS X
 	alias ls='ls -lh --color=auto'
   alias e='gvim'
@@ -33,13 +30,3 @@ alias -g ....='../../..'
 
 alias g='git'
 alias -g gr='`git rev-parse --show-cdup`'
-
-alias gcb="echo `git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`"
-# alias gmm="export GIT_BRANCH=`gcb` && g co master && g pl && g co $GIT_BRANCH && g m master && g s"
-
-# AnalogAnalytics Stuff
-alias hstat='for S in 0 1 2; do; echo "hydra$S"; ssh hydra$S.analoganalytics.net "ps aux | grep hydra | grep -v grep; vmstat"; done'
-
-# Rails Stuff
-alias db_migrate='rake db:migrate && rake db:migrate:redo && rake db:test:prepare'
-
